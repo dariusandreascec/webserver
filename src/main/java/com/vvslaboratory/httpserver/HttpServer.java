d065d0d529ed00f639f59aed90f9930b3f3fad08
Andreascec Darius <dariusandreascec@yahoo.com>
2020-11-07 15:37:34 +0200

    First socket run

--- src/main/java/com/vvslaboratory/httpserver/HttpServer.java
+++ src/main/java/com/vvslaboratory/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package com.vvslaboratory.httpserver;
 import com.vvslaboratory.httpserver.config.Configuration;
 import com.vvslaboratory.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * Driver Class for the Http Server
@@ -19,5 +25,37 @@ public class HttpServer {
 
         System.out.println("Using Port: " + conf.getPort());
         System.out.println("Using WebRoot: " + conf.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept(); // prompts the socket that is listening to a port to accept any connections that arrives
+
+            InputStream inputStream = socket.getInputStream(); // reading something from the socket
+            OutputStream outputStream = socket.getOutputStream();
+
+            // TODO we would read
+
+            // TODO we would writing
+            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my HTTP Server</h1></body></html>";
+
+            final String CRLF = "\n\r"; // 13, 10 ASCII
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF + // Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                    "Content-Length: " + html.getBytes().length + CRLF +  // HEADER
+                            CRLF +
+                            html +
+                            CRLF + CRLF ;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
