0a9c36c5170e8efb6ab55ba756aedfb422588105
Andreascec Darius <dariusandreascec@yahoo.com>
2020-11-08 00:23:39 +0200

    First step multithreading

--- src/main/java/com/vvslaboratory/httpserver/HttpServer.java
+++ src/main/java/com/vvslaboratory/httpserver/HttpServer.java
@@ -1,14 +1,15 @@
 package com.vvslaboratory.httpserver;
 
+import com.sun.security.ntlm.Server;
 import com.vvslaboratory.httpserver.config.Configuration;
 import com.vvslaboratory.httpserver.config.ConfigurationManager;
+import com.vvslaboratory.httpserver.core.ServerListenerThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 
+    
 /**
  *
  * Driver Class for the Http Server
@@ -16,46 +17,25 @@ import java.net.Socket;
  */
 
 public class HttpServer {
-    public static void main(String [] args){
 
-        System.out.println("Server starting...");
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
+
+    public static void main(String[] args){
+
+        LOGGER.info("Server starting...");
 
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
 
-        System.out.println("Using Port: " + conf.getPort());
-        System.out.println("Using WebRoot: " + conf.getWebroot());
+        LOGGER.info("Using Port: " + conf.getPort());
+        LOGGER.info("Using WebRoot: " + conf.getWebroot());
 
         try {
-            ServerSocket serverSocket = new ServerSocket(conf.getPort());
-            Socket socket = serverSocket.accept(); // prompts the socket that is listening to a port to accept any connections that arrives
-
-            InputStream inputStream = socket.getInputStream(); // reading something from the socket
-            OutputStream outputStream = socket.getOutputStream();
-
-            // TODO we would read
-
-            // TODO we would writing
-            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my HTTP Server</h1></body></html>";
-
-            final String CRLF = "\n\r"; // 13, 10 ASCII
-
-            String response =
-                    "HTTP/1.1 200 OK" + CRLF + // Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
-                    "Content-Length: " + html.getBytes().length + CRLF +  // HEADER
-                            CRLF +
-                            html +
-                            CRLF + CRLF ;
-
-            outputStream.write(response.getBytes());
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            serverSocket.close();
-
+            ServerListenerThread serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
         } catch (IOException e) {
             e.printStackTrace();
+            // TODO handle
         }
     }
 }
--- src/main/java/com/vvslaboratory/httpserver/core/ServerListenerThread.java
+++ src/main/java/com/vvslaboratory/httpserver/core/ServerListenerThread.java
@@ -1,5 +1,66 @@
 package com.vvslaboratory.httpserver.core;
 
-public class ServerListenerThread {
-    
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class ServerListenerThread extends Thread {
+
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ServerListenerThread.class);
+
+    private int port;
+    private String webroot;
+    private ServerSocket serverSocket;
+
+    public ServerListenerThread(int port, String webroot) throws IOException {
+        this.port = port;
+        this.webroot = webroot;
+        this.serverSocket = new ServerSocket(this.port);
+    }
+
+    @Override
+    public void run() {
+
+        try {
+
+            Socket socket = serverSocket.accept(); // prompts the socket that is listening to a port to accept any connections that arrives
+
+            LOGGER.info(" * Connection accepted: " + socket.getInetAddress() );
+
+            InputStream inputStream = socket.getInputStream(); // reading something from the socket
+            OutputStream outputStream = socket.getOutputStream();
+
+            // TODO we would read
+
+            // TODO we would writing
+            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my HTTP Server</h1></body></html>";
+
+            final String CRLF = "\n\r"; // 13, 10 ASCII
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF + // Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                            "Content-Length: " + html.getBytes().length + CRLF +  // HEADER
+                            CRLF +
+                            html +
+                            CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
